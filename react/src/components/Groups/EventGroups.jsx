788b89fe5686856641b8314b37e266985dcf0cbc
raugdae <raugdae@gmail.com>
2025-10-18 12:21:23 +0200

    modal group

--- react/src/components/Groups/EventGroups.jsx
+++ react/src/components/Groups/EventGroups.jsx
@@ -1,6 +1,6 @@
-import { Trash2, Pen } from 'lucide-react'
+import { Trash2, Pen,X } from 'lucide-react'
 import { useState, useEffect } from 'react'
-import { getEventGroups, deleteEventGroup,updateEventGroup } from '../../api/event';
+import { getEventGroups, deleteEventGroup, updateEventGroup } from '../../api/event';
 import Modal from '../ui/Modal'
 
 
@@ -8,11 +8,12 @@ function EventGroups() {
   const [dataset, setDataset] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedItem, setSelectedItem] = useState(null);
+  const [modalType, setModalType] = useState(null);
 
   const fetchData = async () => {
     const result = await getEventGroups(localStorage.getItem('eventID'));
     await setDataset(result.data)
-  
+
   };
 
 
@@ -28,24 +29,26 @@ function EventGroups() {
 
 
 
-  const handleEdit = async (item) => {
+  const handleEdit = async (item, modalType) => {
     await setSelectedItem(item);
+    await setModalType(modalType)
     console.log(item);
     setIsModalOpen(true);
   };
 
   const closeModal = () => {
     setIsModalOpen(false);
-    setSelectedItem(null);
+    //setSelectedItem(null);
   };
 
-  const handleGroupEditChange = async () =>{
-    console.log ('Update groupe modificaitons', selectedItem);
+  const handleGroupEditChange = async () => {
+    console.log('Update groupe modificaitons', selectedItem);
 
     //const data = {groupe:e.groupe,parentid:e.id}
 
-    const response = await updateEventGroup(localStorage.getItem('eventID'),selectedItem.id,selectedItem);
-  
+    const response = await updateEventGroup(localStorage.getItem('eventID'), selectedItem.id, selectedItem);
+    setIsModalOpen(false)
+
   }
 
 
@@ -59,19 +62,32 @@ function EventGroups() {
     }
   }
 
+  const handleAdd = async(modalType) => {
+    await setModalType(modalType);
+    setIsModalOpen(true);
+  }
+
 
 
 
   return (
     <div className="flex flex-col m-4">
+      <div className='flex justify-center '>
+      <button className='border-lime-800 border-4 bg-emerald-700 hover:bg-green-300 ' onClick={()=>handleAdd('Add')}>Ajouter un groupe</button>
+      </div>
       {dataset.length > 0 && (
         <div className="flex flex-row m-4 justify-around bg-gray-100 rounded">
-          {Object.keys(dataset[0]).map((key, index) => (
-            
-            <div key={index} className="font-bold flex-1 text-center">
-              {key}
-            </div>
-          ))}
+          {Object.keys(dataset[0]).map((key, index) => {
+
+            if (key !== 'id' && key !== 'fk_parentgroupid') {
+              return (
+                <div key={index} className="font-bold flex-1 text-center">
+                  {key}
+                </div>
+              )
+            }
+
+          })}
           <div className="font-bold w-24 text-center">Actions</div>
         </div>
       )}
@@ -79,17 +95,19 @@ function EventGroups() {
       {dataset.map((item, index) => (
         <div key={index} className="flex flex-row m-2 justify-around border-b items-center">
           {Object.values(item).map((value, i) => {
-            if (value !== 'id' && value !== 'fk_parentgroupid')
-            return (
-            <div key={i} value={value} className="flex-1 text-center">
-              {value}
-            </div>)
+
+
+            if (Object.keys(item)[i] !== 'id' && Object.keys(item)[i] !== 'fk_parentgroupid')
+              return (
+                <div key={i} value={value} className="flex-1 text-center">
+                  {value}
+                </div>)
           })}
           <div className="flex gap-2">
 
             <div
               className='border-2 p-2 bg-orange-500 hover:bg-green-500 cursor-pointer rounded'
-              onClick={() =>handleEdit(item)}
+              onClick={() => handleEdit(item, 'Edit')}
             >
               <Pen size={18} />
             </div>
@@ -110,9 +128,15 @@ function EventGroups() {
       <Modal
         isOpen={isModalOpen}
         onClose={closeModal}
-        title="Éditer le groupe"
+        
       >
-        {selectedItem && (
+        {modalType === 'Edit' && (
+          <div>
+          <div className='flex justify-between item-center mb-4 bg-violet-400'>
+                    <div className="flex font-extrabold p-4"><h2 className='font-normal p-4 align-middle
+                    75'>Modifier le groupe </h2> {selectedItem.groupe}</div>
+                    <button onClick={closeModal} className='text-gray-500 hover:text-red-700 transition-colors'><X/></button>
+                </div>
           <div className="space-y-4">
             {Object.keys(selectedItem).map((key) => {
               if (key === 'groupe') {
@@ -131,27 +155,30 @@ function EventGroups() {
                   />
                 </div>)
               }
-              if (key === 'parent'){
-              return (<div>
-                <div className='block text-sm text-gray-700 mb-1'>
-                  <h2>Parent</h2>
+              if (key === 'parent') {
+                return (<div>
+                  <div className='block text-sm text-gray-700 mb-1'>
+                    <h2>Parent</h2>
                   </div>
-              <select className='rounded-2xl, bg-cyan-300 border-2' onChange={(e) => {
-                      console.log(key
-                      )
-                      setSelectedItem({
+                  <select className='rounded-2xl, bg-cyan-300 border-2' onChange={(e) => {
+                    console.log(key
+                    )
+                    setSelectedItem({
                       ...selectedItem,
-                      fk_parentgroupid:e.target.value})}}>
-                {dataset.map((item,index) => {  
-                  
-                                     
-                    return <option 
-                    key={index} className='bg-green-400 ' value={item.id}>{item.groupe}</option>
+                      fk_parentgroupid: e.target.value
+                    })
+                  }}>
+                    {dataset.map((item, index) => {
 
-                })}
-              </select>
-            </div>)}
-              
+                      if (item.groupe !== selectedItem.groupe)
+                        return <option
+                          key={index} className='bg-green-400 ' value={item.id}>{item.groupe}</option>
+
+                    })}
+                  </select>
+                </div>)
+              }
+
             })}
 
             <div className="flex gap-2 justify-end mt-6">
@@ -169,7 +196,18 @@ function EventGroups() {
               </button>
             </div>
           </div>
-        )}
+        </div>
+        )
+
+        }
+        {
+        modalType === 'Add' && (
+        <div className='flex flex-col'>
+          <h2 className='justify-center'>Ajouter</h2>
+        </div>
+        )
+
+        }
       </Modal>
     </div>
   );
--- react/src/components/ui/Modal.jsx
+++ react/src/components/ui/Modal.jsx
@@ -1,15 +1,11 @@
 import {X} from 'lucide-react'
 
-function Modal({onClose,isOpen,title,children}){
+function Modal({onClose,isOpen,children}){
     if (!isOpen) return null;
 
     return(
         <div onClick={onClose} className='fixed inset-0 bg-black flex item-center justify-center align-middle'>
             <div onClick={(e) => e.stopPropagation()} className='bg-white p-4  max-w-md w-full mx-4 relative h-1/2'>
-                <div className='flex justify-between item-center mb-4'>
-                    <h2>{title}</h2>
-                    <button onClick={onClose} className='text-gray-500 hover:text-red-700 transition-colors'><X/></button>
-                </div>
                 <div>
                     {children}
                 </div>
